2db6f46d4feafcd6beb674a30af1be5e4fbd8434
usernameisMan <vip939950559@qq.com>
2019-11-28 14:01:02 +0800

    feat: 加入设备类型页面

--- src/app.tsx
+++ src/app.tsx
@@ -8,30 +8,17 @@ import './icon.scss'
 import './app.scss'
 import CreateMessage from './pages/createMessage/createMessage'
 
-// 如果需要在 h5 环境中开启 React Devtools
-// 取消以下注释：
-// if (process.env.NODE_ENV !== 'production' && process.env.TARO_ENV === 'h5')  {
-//   require('nerv-devtools')
-// }
-
 const store = {
   counterStore
 }
 
 class App extends Component {
-
-  /**
-   * 指定config的类型声明为: Taro.Config
-   *
-   * 由于 typescript 对于 object 类型推导只能推出 Key 的基本类型
-   * 对于像 navigationBarTextStyle: 'black' 这样的推导出的类型是 string
-   * 提示和声明 navigationBarTextStyle: 'black' | 'white' 类型冲突, 需要显示声明类型
-   */
   config: Config = {
     pages: [
       'pages/list/list',
       'pages/createMessage/createMessage',
       'pages/user/user',
+      'pages/deviceType/deviceType',
     ],
     window: {
       backgroundTextStyle: 'light',
@@ -42,11 +29,11 @@ class App extends Component {
     tabBar:{
       selectedColor: '#6190e8',
       list: [
-        { 
+        {
           pagePath: 'pages/list/list',
           text: '列表',
         },
-        { 
+        {
           pagePath: 'pages/user/user',
           text: '我的',
         },
