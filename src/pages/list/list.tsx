a238b9ba682c3ea8eea8638f6d14e6915023a683
usernameisMan <vip939950559@qq.com>
2019-12-09 20:02:53 +0800

    feat: 修改list样式

--- src/pages/list/list.tsx
+++ src/pages/list/list.tsx
@@ -17,7 +17,8 @@ interface ICard {
   time: string,
   title: string,
   description: string,
-  type: 'NEED' | 'IDLE'
+  type: 'NEED' | 'IDLE',
+  phoneNumber: string | number
 }
 
 interface List {
@@ -116,12 +117,19 @@ class List extends Component<IListProps, IListState> {
     })
   }
 
+  phoneCall (phoneNumber) {
+    Taro.makePhoneCall({
+      phoneNumber
+    })
+  }
+
   renderCard(card: ICard) {
     const {
       title,
       description,
       type,
-      time
+      time,
+      phoneNumber,
     } = card
 
     if (type === 'NEED') {
@@ -132,19 +140,21 @@ class List extends Component<IListProps, IListState> {
       className='card'
       note=''
       extra={time}
+      key={title}
       title={title}
       thumb='http://img10.360buyimg.com/jdphoto/s72x72_jfs/t5872/209/5240187906/2872/8fa98cd/595c3b2aN4155b931.png'
     >
     {description}
-    <Image
-          style='width: 300px;height: 100px;background: #fff;'
-          src='https://camo.githubusercontent.com/3e1b76e514b895760055987f164ce6c95935a3aa/687474703a2f2f73746f726167652e333630627579696d672e636f6d2f6d74642f686f6d652f6c6f676f2d3278313531333833373932363730372e706e67'
-        />
+      <Image
+        className='img'
+        src='https://camo.githubusercontent.com/3e1b76e514b895760055987f164ce6c95935a3aa/687474703a2f2f73746f726167652e333630627579696d672e636f6d2f6d74642f686f6d652f6c6f676f2d3278313531333833373932363730372e706e67'
+      />
       <View className="at-row row-call at-row__justify--between">
         <AtButton
           className="at-col at-col-12"
           type='primary'
           size='small'
+          onClick={()=> {this.phoneCall(String(phoneNumber))}}
         >拨打电话</AtButton>
       </View>
     </AtCard>
