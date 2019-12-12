a974b95e7467860a3562cb5463bb1d634b26befe
usernameisMan <vip939950559@qq.com>
2019-12-12 16:22:14 +0800

    feat: 表单验证器和配置

---
+++ src/pages/createMessage/validation.tsx
@@ -0,0 +1,45 @@
+
+import { ICreatemessageState } from './createMessage'
+
+var validate = require("validate.js");
+
+var constraints : Record<keyof ICreatemessageState, any> = {
+  title: {
+    presence: true,
+    length: {
+      minimum: 5,
+      maximum: 20,
+      message: "^标题长度 5 - 20"
+    }
+  },
+  description:{
+    presence: true,
+    length: {
+      minimum: 5,
+      maximum: 150,
+      message: "^标题长度 5 - 20"
+    }
+  },
+  dateSel: {
+    presence: true,
+  },
+  phoneNumber: {
+    presence: true,
+    format: {
+      pattern: /^1[3456789]\d{9}$/,
+      message: "^手机号格式错误"
+    }
+  },
+  status: {
+    presence: true,
+  },
+  files: {},
+  lat: {
+    presence: true,
+  },
+  lng: {
+    presence: true,
+  },
+};
+
+export default (data: ICreatemessageState) => validate(data, constraints)
